b4cb481e7dd5b473c75f915fcfa575c0f8834ce7
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2023-07-17 13:23:40 +0600

    House hunter basic setup created

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className=" text-2xl text-textColor">Mahedi</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#5cd183',
+        secondary: '#afd6e9',
+        accent: '#2d9f53',
+        bgColor: '#fefbfd',
+        textColor: '#1c0815'
+      },
+    },
+  },
+  plugins: [],
+}
+
